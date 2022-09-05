24b087d364e35291f9944eb88926ca1c73a80a99
Ferenc Hammerl <fhammerl@github.com>
2022-09-05 16:16:20 +0200

    Try codespaces

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,12 +1,9 @@
-### Create _dotnetsdk
 echo 'Creating symlink for embedded sdk...'
-ln -s /home/codespace/.dotnet/sdk/ _dotnetsdk 
-echo .6.0.300 >> _dotnetsdk/6.0.300/.6.0.300
-ln -s /home/codespace/.dotnet/dotnet _dotnetsdk/6.0.300/dotnet
 
-### Restore 
-dotnet restore src/Runner.Listener &
-dotnet restore src/Runner.Common &
-dotnet restore src/Runner.Sdk &
-dotnet restore src/Runner.Worker &
-dotnet restore src/Test &
+# pwd is the source root folder
+ln -s /home/root/.dotnet/sdk/ _dotnetsdk 
+echo .6.0.300 >> _dotnetsdk/6.0.300/.6.0.300
+ln -s /home/root/.dotnet/dotnet _dotnetsdk/6.0.300/dotnet
+
+cd src
+./dev.sh

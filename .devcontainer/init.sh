d3377984923fd37ef0d1eefccc8fc079c568e4c0
Ferenc Hammerl <fhammerl@github.com>
2022-09-06 16:13:00 +0200

    Use dind devcontainer with correct dotnet sdk

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 echo 'Creating symlink for embedded sdk...'
 
 # pwd is the source root folder
-ln -s /home/root/.dotnet/sdk/ _dotnetsdk 
+ln -s /usr/share/dotnet/sdk _dotnetsdk 
 echo .6.0.300 >> _dotnetsdk/6.0.300/.6.0.300
-ln -s /home/root/.dotnet/dotnet _dotnetsdk/6.0.300/dotnet
+# ln -s /home/root/.dotnet/dotnet _dotnetsdk/6.0.300/dotnet
+
+exit 0
 
 cd src
 ./dev.sh

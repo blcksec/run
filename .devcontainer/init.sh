410e303c5f8e332c99736de7bed88534cc3cd398
Ferenc Hammerl <fhammerl@github.com>
2022-09-12 12:05:25 +0000

    Better init (no restore + rerunnable)

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -4,13 +4,15 @@ echo 'Creating symlink for embedded sdk...'
 
 # pwd is the source root folder
 rm -rf _dotnetsdk 
-ln -s /usr/share/dotnet/sdk/6.0.300 _dotnetsdk 
+mkdir _dotnetsdk
+ln -s /usr/share/dotnet/sdk/6.0.300 _dotnetsdk/6.0.300
 
 # Satisfy dev.sh SDK conditions
-echo .6.0.300 > _dotnetsdk/6.0.300/.6.0.300
-ln -s /usr/bin/dotnet /usr/share/dotnet/sdk/6.0.300/dotnet
+echo .6.0.300 > _dotnetsdk/.6.0.300
+ln -s -f /usr/bin/dotnet /usr/share/dotnet/sdk/6.0.300/dotnet
 
+rm -rf _layout
 cd src
-./dev.sh
+./dev.sh l
 
-dotnet restore ActionsRunner.sln
+code README.md

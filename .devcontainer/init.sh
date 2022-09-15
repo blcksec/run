f6751205e4cb38a8c0f77592f4eda8b5b69a6547
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2022-09-15 09:35:58 +0000

    Init as sudo

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo 'Creating symlink for embedded sdk...'
-
+whoami
+pwd 
 # pwd is the source root folder
 rm -rf _dotnetsdk 
 mkdir _dotnetsdk
 ln -s /usr/share/dotnet/sdk/6.0.300 _dotnetsdk/6.0.300
-
 # Satisfy dev.sh SDK conditions
-echo .6.0.300 > _dotnetsdk/.6.0.300
+echo .6.0.300 > _dotnetsdk/6.0.300/.6.0.300
 ln -s -f /usr/bin/dotnet /usr/share/dotnet/sdk/6.0.300/dotnet
 
 rm -rf _layout

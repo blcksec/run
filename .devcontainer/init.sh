3cc465d610c228769450f42ae6120575119500be
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2022-09-15 09:45:10 +0000

    Echo in init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -11,11 +11,13 @@ ln -s /usr/share/dotnet/sdk/6.0.300 _dotnetsdk/6.0.300
 echo .6.0.300 > _dotnetsdk/6.0.300/.6.0.300
 ln -s -f /usr/bin/dotnet /usr/share/dotnet/sdk/6.0.300/dotnet
 
+echo "Building '_layout'"
 rm -rf _layout
 cd src
 ./dev.sh l
 
 # dev.sh doesn't restore 'Test'
+echo 'Restoring Test project'
 cd Test
 dotnet restore 
 
